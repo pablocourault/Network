5daf8826d5e78132beeab3dcd721ea298e7ba067
pablocourault <pablocourault@gmail.com>
2021-03-24 21:26:31 -0300

    final version

---
+++ network/static/network/register.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    var altura = window.innerHeight;
+    
+    document.querySelector('#registerbody').style.height = altura+'px';
+  
+    
+});
